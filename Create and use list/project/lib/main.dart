7686c798e06be8747f92443e19d79bd9d5587502
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-21 18:27:02 +0530

    Im Still naive

--- Create and use list/project/lib/main.dart
+++ Create and use list/project/lib/main.dart
@@ -18,12 +18,23 @@ class MyApp extends StatelessWidget {
       title: title,
       home: Scaffold(
         appBar: AppBar(
-          title: const Row(
-            children: [
-              Spacer(),
-              Text(title)
-            ],
-          ),            
+          title: const Text(title),          
+        ),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text("Map"),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text("Album"),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text("Phone"),
+            ),
+          ],
         ),
       ),
     );
