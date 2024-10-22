4ee16401960c53cd182381100905788a83cabbf4
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-22 17:32:44 +0530

    Not enough need more practice

--- List with spaced items/project/lib/main.dart
+++ List with spaced items/project/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,14 +12,51 @@ class MyApp extends StatelessWidget {
 
     const title = "List With Spaced Items";
 
+    const items = 7;
+
     return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
 
       title: title,
 
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(title),
+          title: const Text(title, style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.red,
+          centerTitle: true,
         ),
+        body: LayoutBuilder(builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: List.generate(items, (index) => ItemWidget(text: 'item $index')),
+              ),
+              )
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(child: Text(text),),
       ),
     );
   }
