974f35d307f6c1c0b49961f772759648d165b0ea
Yuslash CrimsonConor <160831809+Yuslash@users.noreply.github.com>
2024-10-24 11:40:56 +0530

    Here we go

--- New screen and back/project/lib/main.dart
+++ New screen and back/project/lib/main.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Navigation Basics",
+    home: FirstRoute(),  
+  ),);
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-      const title = "New Screen and Back";
-    return MaterialApp(
-      title: title,
-      debugShowCheckedModeBanner: false,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Navigation One"),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const SecoundRoute(),),);
+        }
+        , child: const Text("Open Route")),
+      ),
+    );
+  }
 
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(title),
-        ),
+}
+
+class SecoundRoute extends StatelessWidget {
+  
+  const SecoundRoute({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Navigation"),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          Navigator.pop(context);
+        }, child: const Text("Go Back!")),
       ),
     );
   }
